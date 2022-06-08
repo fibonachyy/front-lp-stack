c854874f7a275b35142dd89228e2a6b6dac114b2
Cihan Mehdi Gül <mehdi.ashouri@demarkelabs.com>
2022-06-08 09:34:37 +0300

    update contrCT

--- src/farm/lib/constants.js
+++ src/farm/lib/constants.js
@@ -1,10 +1,10 @@
 // 4 - Rinkeby
 export const contractAddresses = {
   erc20: {
-    4: '0x21BBDcbbf0679EB0230D1f50Bb53460ec2AE9786',
+    4: '0x003B992a34D8b5a64CFf5Ea110d8158C4d075c70',
   },
   farm: {
-    4: '0x0a1b05fd6250bEE2bf85C547817082E85fEe268C',
+    4: '0xEB1A9E02b28D14626F1928e946a1727Bdf18809B',
   },
   weth: {
     4: '0x2fcc4dba284dcf665091718e4d0dab53a416dfe7',
@@ -15,10 +15,10 @@ export const supportedPools = [
   {
     id: 'ltov1',
     version: 'V1',
-    name: 'YESP ERC-20',
+    name: 'LTO ERC-20',
     pid: 0,
     lpAddresses: {
-      4: '0x200fD413377a2dD2F2D6Ffc4814F190AF0609F4e',
+      4: '0x8cB08787a8e176cCcB29C95DacBdb05eBEd7ED72',
     },
     tokenAddresses: { ...contractAddresses.erc20 },
     symbol: 'yesp-ETH LP',
